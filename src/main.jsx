10dae91ec07507d1a7c4d73e7f8c80a85ac714f2
Adriano Mendes <adriano.alyson@hotmail.com>
2023-08-19 09:49:52 -0300

    subindo

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Login from './pages/login/login.jsx'
+import Register from './pages/register/register.jsx'
+import ErrorPage from './pages/errorPage/error-page.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:"login",
+    element: <Login/>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path:"register",
+    element:<Register/>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
