19ec946ce7747b056c10abc26933ccdea4b514a5
Adriano Mendes <39601251+AdrianoLMendes@users.noreply.github.com>
2023-08-24 17:57:43 -0300

    header home

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import Login from './pages/login/login.jsx'
-import Register from './pages/register/register.jsx'
-import ErrorPage from './pages/errorPage/error-page.jsx'
+import Login from "./pages/login/login.jsx";
+import Register from "./pages/register/register.jsx";
+import ErrorPage from "./pages/errorPage/error-page.jsx";
+import Home from "./pages/home/home.jsx";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <Login/>,
-    errorElement: <ErrorPage/>
+    path: "/",
+    element: <Login />,
+    errorElement: <ErrorPage />,
   },
   {
-    path:"register",
-    element:<Register/>
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "home",
+    element: <Home />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
