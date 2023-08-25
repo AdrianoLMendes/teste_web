5f9e7843afd8ba90410aa5589379d4a3db857602
Adriano Mendes <39601251+AdrianoLMendes@users.noreply.github.com>
2023-08-25 12:34:22 -0300

    pagina register reformulada

--- src/pages/errorPage/error-page.jsx
+++ src/pages/errorPage/error-page.jsx
@@ -1,31 +1,41 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+
 import "./error-page.css";
 
 function ErrorPage() {
   return (
     <>
-      <section className="page_404">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12 ">
-              <div className="col-sm-10 col-sm-offset-1  text-center">
-                <div className="four_zero_four_bg">
-                  <h1 className="text-center ">404</h1>
-                </div>
-
-                <div className="contant_box_404">
-                  <h3 className="h2">Parece que você está perdido...</h3>
-
-                  <p>a página que você está procurando não está disponível!!</p>
-
-                  <a href="/" className="link_404">
-                    voltar
-                  </a>
+      <Container maxWidth="sm">
+        <section className="page_404">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 ">
+                <div className="col-sm-10 col-sm-offset-1  text-center">
+                  <div className="four_zero_four_bg">
+                    <h1 className="text-center">404</h1>
+                  </div>
+                  <div className="contant_box_404">
+                    <h2 className="h2">Parece que você está perdido...</h2>
+                    <p>
+                      a página que você está procurando não está disponível!!
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+        <Grid className="link">
+          <Grid>
+            <Link href="/" underline="none" variant="body2">
+              Ir para login
+            </Link>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,43 +1,58 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-// TODO remove, this demo shouldn't need to reset the theme.
 
 const defaultTheme = createTheme();
 
-export default function SignUp() {
+export default function SignInSide() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+      email: data.get('email'),
+      password: data.get('password'),
     });
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
+      <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
-        <Box
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
           sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
           }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
@@ -109,8 +124,9 @@ export default function SignUp() {
               </Grid>
             </Grid>
           </Box>
-        </Box>
-      </Container>
+          </Box>
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
